7a0fee0593554f54cfdfbaa61c4fd786b5e0f3e5
Unamed <34691959+Unam3dd@users.noreply.github.com>
2020-04-22 16:34:32 +0200

    Delete windb_wininet.h

--- WinBackdoorLib0.5/includes/windb_wininet.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <wininet.h>
-
-using namespace std;
-
-int download_on_ftp(char* filename, char* ftp_host, int ftp_port, char* ftp_user, char* ftp_pass);
-int download_to_ftp(char* filename, char* ftp_host, int ftp_port, char* ftp_user, char* ftp_pass);
